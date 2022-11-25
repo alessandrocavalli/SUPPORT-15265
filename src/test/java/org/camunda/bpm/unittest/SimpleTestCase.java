45167d067ac701e84051e8eb683340cf595ee6f2
Alessandro Cavalli <alessandro.cavalli@camunda.com>
2022-11-25 12:39:09 +0100

    first commit

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -18,10 +18,12 @@ package org.camunda.bpm.unittest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processInstanceQuery;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -43,18 +45,25 @@ public class SimpleTestCase {
   @Deployment(resources = {"testProcess.bpmn"})
   public void shouldExecuteProcess() {
     // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
+	  
+	  Map<String, Object> variables=new HashMap<>();
+      variables.put("elementId","hello");
+
+      ProcessInstance processInstance = runtimeService()
+		.startProcessInstanceByKey("handle-element", variables);
+	  
     // Then it should be active
     assertThat(processInstance).isActive();
     // And it should be the only instance
     assertThat(processInstanceQuery().count()).isEqualTo(1);
     // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    // cancel-element-${execution.getVariable("elementId")}
+    // since of the above expression, the event name should be : cancel-element-hello, but it only will be cancel-element-
+    
+    assertEquals(1, runtimeService().createEventSubscriptionQuery().eventType("signal").eventName("cancel-element-hello").count());
+    
+    
   }
 
 }
